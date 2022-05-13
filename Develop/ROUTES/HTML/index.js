efc2a31d0e4b0d6ccad29c327f9a5ff7c7f0c5e3
cruiz328 <lemmy125@gmail.com>
2022-05-13 12:54:44 -0700

    edited html route

--- Develop/ROUTES/HTML/index.js
+++ Develop/ROUTES/HTML/index.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-// GET landing page
-router.get('/', (req, res) => {
-    res.render('login', {
-        layout: 'login'
-    })
-})
-
 //GET dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
+router.get('/dashboard', (req, res) => {
     res.render('dashboard')
 })
 module.exports = router
